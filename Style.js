3fca6b673f1c2cd9caca4ce267c99718f09d7cff
Jibin <jibingallistus@gmail.com>
2022-09-14 12:06:34 +0400

    Allow number of squares to change without changing container size

--- Style.js
+++ Style.js
@@ -1,5 +1,46 @@
 
+const container = document.querySelector('.container');
+
+let numberOfSquaresOnOneSide = 100;
 
 
-const div = document.createElement('div');
-div.setAttribute('border','solid')
+container.style.gridTemplateColumns =`repeat(${numberOfSquaresOnOneSide},1fr)`;
+
+
+for (let i = 0; i < numberOfSquaresOnOneSide*numberOfSquaresOnOneSide; i++) {
+
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div)
+
+}
+
+
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => { 
+    
+    
+    
+    square.addEventListener('mouseenter',  () => {
+
+        console.log('entered');
+        square.classList.add('entered');
+
+    })
+
+ 
+
+
+
+
+
+});
+
+
+
+
+
+
+
